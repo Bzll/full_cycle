1f347fe9c07d772dceb5d6ae83b33a7a12e29252
ciro.chagas <ciro.chagas@agroamazonia.com>
2021-07-28 09:47:50 -0400

    Desafio 02

---
+++ docker_desafio_02/node/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    database: 'nodedb', 
+    user: 'root',
+    password: 'root'
+};
+const mysql = require('mysql');
+var connection = mysql.createConnection(config);
+
+var sql = `create table if not exists people(id int not null auto_increment, name varchar(255), primary key(id));`;
+connection.query(sql);
+sql = `INSERT INTO people(name) values ('Ciro')`;
+connection.query(sql);
+
+connection.end();
+
+app.get('/',(req,res) => {
+    var response = ('<h1>Full Cycle</h1>')
+
+    connection = mysql.createConnection(config)
+    connection.query("SELECT * FROM people", function(err, result, fields){
+        if (err) throw err;
+        response += '<br>' + result[0]['name'];
+        res.send(response);
+    })
+})
+
+app.listen(port, () =>{
+    console.log('Rodando na porta ' + port)
+})
